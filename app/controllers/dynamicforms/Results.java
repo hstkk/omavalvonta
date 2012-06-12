d52f890080a04d3c99a193f36709e025f63aef7c
Sami <hostikka@gmail.com>
2012-06-12 15:31:08 +0300

    Added routes to dynamic results

--- app/controllers/dynamicforms/Results.java
+++ app/controllers/dynamicforms/Results.java
@@ -1,7 +1,28 @@
 package controllers.dynamicforms;
 
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Results extends Controller {
 
+	@Transactional(readOnly = true)
+	public static Result add(Long batchId, String program) {
+		return TODO;
+	}
+
+	@Transactional
+	public static Result save(Long batchId, String program) {
+		return TODO;
+	}
+
+	@Transactional(readOnly = true)
+	public static Result show(Long batchId, String program) {
+		return TODO;
+	}
+
+	@Transactional(readOnly = true)
+	public static Result history(Long batchId, String program) {
+		return TODO;
+	}
 }
