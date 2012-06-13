db5ff02f6dfdc1002d2b31bae2e7446a9c689ac8
Sami <hostikka@gmail.com>
2012-06-13 08:22:36 +0300

    Added numbers to formify

--- app/utils/Form.java
+++ app/utils/Form.java
@@ -12,6 +12,9 @@ public class Form {
 	public static <T> String formify(List<T> list) {
 		if (list == null || list.isEmpty())
 			return null;
+		
+		int i = 1;
+		
 		StringBuilder html = new StringBuilder();
 		for (Object e : list) {
 			Field field = null;
@@ -34,32 +37,32 @@ public class Form {
 				html.append("</label><div class=\"controls\">");
 				switch (field.type) {
 				case CHECKBOX:
-					html.append("<input type=\"checkbox\" name=\"values[].value\" value=\"");
+					html.append("<input type=\"checkbox\" name=\"values[" + i + "].value\" value=\"");
 					html.append(value);
 					html.append("\"/>");
 					break;
 				case DATE:
-					html.append("<input class=\"input-xlarge\" name=\"values[].value\" type=\"text\" placeholder=\"pp.kk.vvvv\" value=\"");
+					html.append("<input class=\"input-xlarge\" name=\"values[" + i + "].value\" type=\"text\" placeholder=\"pp.kk.vvvv\" value=\"");
 					html.append(value);
 					html.append("\"/>");
 					break;
 				case INT:
-					html.append("<input class=\"input-xlarge\" name=\"values[].value\" type=\"text\" placeholder=\"0\" value=\"");
+					html.append("<input class=\"input-xlarge\" name=\"values[" + i + "].value\" type=\"text\" placeholder=\"0\" value=\"");
 					html.append(value);
 					html.append("\"/>");
 					break;
 				case DOUBLE:
-					html.append("<input class=\"input-xlarge\" name=\"values[].value\" type=\"text\" placeholder=\"0,0\" value=\"");
+					html.append("<input class=\"input-xlarge\" name=\"values[" + i + "].value\" type=\"text\" placeholder=\"0,0\" value=\"");
 					html.append(value);
 					html.append("\"/>");
 					break;
 				case TEXT:
-					html.append("<input class=\"input-xlarge\" name=\"values[].value\" type=\"text\" value=\"");
+					html.append("<input class=\"input-xlarge\" name=\"values[" + i + "].value\" type=\"text\" value=\"");
 					html.append(value);
 					html.append("\"/>");
 					break;
 				case TEXTAREA:
-					html.append("<textarea class=\"input-xlarge\" name=\"values[].value\" rows=\"3\">");
+					html.append("<textarea class=\"input-xlarge\" name=\"values[" + i + "].value\" rows=\"3\">");
 					html.append(value);
 					html.append("</textarea>");
 					break;
@@ -71,13 +74,14 @@ public class Form {
 				}
 				html.append("</div></div>");
 				if (field.isSigned)
-					html.append("<div class=\"control-group\"><div class=\"controls\"><label class=\"checkbox\"><input type=\"checkbox\" name=\"values[].ack\" value=\"false\"/>Kuittaa</label></div></div>");
+					html.append("<div class=\"control-group\"><div class=\"controls\"><label class=\"checkbox\"><input type=\"checkbox\" name=\"values[" + i + "].ack\" value=\"false\"/>Kuittaa</label></div></div>");
 				else
-					html.append("<input type=\"hidden\" name=\"values[].ack\"/>");
-				html.append("<input type=\"hidden\" name=\"values[].fieldId\" value=\"");
+					html.append("<input type=\"hidden\" name=\"values[" + i + "].ack\"/>");
+				html.append("<input type=\"hidden\" name=\"values[" + i + "].fieldId\" value=\"");
 				html.append(field.id);
-				html.append("\"/><div class=\"control-group\"><div class=\"controls\"><textarea class=\"input-xlarge\" name=\"values[].comment\" rows=\"3\"></textarea><label class=\"help-inline\">Huomautuksia</label></div></div>");
+				html.append("\"/><div class=\"control-group\"><div class=\"controls\"><textarea class=\"input-xlarge\" name=\"values[" + i + "].comment\" rows=\"3\"></textarea><label class=\"help-inline\">Huomautuksia</label></div></div>");
 				html.append("</fieldset>");
+				i++;
 			}
 		}
 		return (html.length() > 0) ? html.toString() : null;
