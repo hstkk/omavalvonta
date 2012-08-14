eda43c42057bddd059773e5b13ec5277232c2387
Sami <hostikka@gmail.com>
2012-08-14 17:43:51 +0300

    Added ingredient entity

---
+++ app/models/Ingredient.java
@@ -0,0 +1,33 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+import play.data.validation.Constraints.Required;
+import play.db.jpa.JPA;
+
+/**
+ * 
+ * @author Sami Hostikka <dev@01.fi>
+ *
+ */
+@Entity
+public class Ingredient extends JpaModel {
+	@Required
+	@NotNull
+	String name;
+
+	String description;
+
+	Integer bestBefore;
+
+	public static Ingredient findById(Long id) {
+		if (id == null)
+			return null;
+		try {
+			return JPA.em().find(Ingredient.class, id);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
