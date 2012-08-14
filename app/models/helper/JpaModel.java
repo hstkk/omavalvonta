86fb87a763a19a18e80d65a7a147f6b0789d28e1
Sami <hostikka@gmail.com>
2012-08-14 18:44:28 +0300

    Remove duplicate page class

--- app/models/helper/JpaModel.java
+++ app/models/helper/JpaModel.java
@@ -47,36 +47,4 @@ public class JpaModel {
 			return false;
 		}
 	}
-
-	public static class Page<T> {
-		private final int size;
-		private final int index;
-		private final List<T> list;
-
-		public Page(List<T> list, int index) {
-			this.size = Play.application().configuration().getInt("page.size");
-			this.list = list;
-			this.index = index;
-		}
-
-		public boolean hasPrevoius() {
-			return index > 1;
-		}
-
-		public boolean hasNext() {
-			return (list.size() / size) >= index;
-		}
-
-		public boolean isEmpty() {
-			return list.isEmpty();
-		}
-
-		public int getIndex() {
-			return index;
-		}
-
-		public List<T> getList() {
-			return list;
-		}
-	}
 }
