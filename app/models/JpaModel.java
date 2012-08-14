bfa0a0e7fb94582ef1d258f5c6da3daea6bc00f8
Sami <hostikka@gmail.com>
2012-08-14 18:28:12 +0300

    Add page

--- app/models/JpaModel.java
+++ app/models/JpaModel.java
@@ -1,6 +1,10 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.*;
+
+import play.Play;
 import play.db.jpa.*;
 
 /**
@@ -19,7 +23,7 @@ public class JpaModel {
 			JPA.em().persist(this);
 			return true;
 		} catch (Exception e) {
-			System.out.println("\n\n"+e+"\n\n");
+			System.out.println("\n\n" + e + "\n\n");
 			e.printStackTrace();
 			return false;
 		}
@@ -41,6 +45,38 @@ public class JpaModel {
 		} catch (Exception e) {
 			System.out.println(e.toString());
 			return false;
+		}
+	}
+
+	public static class Page<T> {
+		private final int size;
+		private final int index;
+		private final List<T> list;
+
+		public Page(List<T> list, int index) {
+			this.size = Play.application().configuration().getInt("page.size");
+			this.list = list;
+			this.index = index;
+		}
+
+		public boolean hasPrevoius() {
+			return index > 1;
+		}
+
+		public boolean hasNext() {
+			return (list.size() / size) >= index;
+		}
+
+		public boolean isEmpty() {
+			return list.isEmpty();
+		}
+
+		public int getIndex() {
+			return index;
+		}
+
+		public List<T> getList() {
+			return list;
 		}
 	}
 }
