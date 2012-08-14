70730a198db750d5cb8d9941f14d47567d2af4ba
Sami <hostikka@gmail.com>
2012-08-15 00:11:44 +0300

    Add queries to FormType entity

--- app/models/dynamicforms/FormType.java
+++ app/models/dynamicforms/FormType.java
@@ -5,7 +5,10 @@ import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 
 import play.data.validation.Constraints.Required;
+import play.Play;
+import play.db.jpa.JPA;
 import models.helpers.JpaModel;
+import models.helpers.Page;
 
 /**
  * 
@@ -23,4 +26,36 @@ public class FormType extends JpaModel {
 	@NotNull
 	@Column(unique = true)
 	public String slug;
+
+	public static FormType findById(Long id) {
+		try {
+			if (id != null)
+				return JPA.em().find(FormType.class, id);
+		} catch (Exception e) {
+		}
+		return null;
+	}
+
+	public String toString() {
+		return name;
+	}
+
+	public static Page page(int index) {
+		try {
+			int size = Play.application().configuration().getInt("page.size");
+			if (index < 1)
+				index = 1;
+			Integer rows = (Integer) JPA.em()
+					.createQuery("select count(*) from FormType")
+					.getSingleResult();
+			List<FormType> list = JPA.em()
+					.createQuery("from FormType i order by i.name asc")
+					.setFirstResult((index - 1) * size).setMaxResults(size)
+					.getResultList();
+			if (rows != null || list != null)
+				return new Page(index, size, rows, list);
+		} catch (Exception e) {
+		}
+		return null;
+	}
 }
