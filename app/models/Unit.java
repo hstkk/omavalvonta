638d30c90e497350ddc96a24bcb0d8643aa69a49
Sami <hostikka@gmail.com>
2012-09-02 04:10:34 +0300

    Added units

--- app/models/Unit.java
+++ app/models/Unit.java
@@ -1,20 +1,74 @@
 package models;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import models.helpers.JpaModel;
+import models.helpers.Page;
+
+import org.hibernate.envers.Audited;
+
+import play.Play;
+import play.data.validation.Constraints.*;
+import play.db.jpa.*;
 
 /**
  * 
  * @author Sami Hostikka <dev@01.fi>
  * 
  */
-public enum Unit {
-	KG, L;
+@Entity
+@Audited
+public class Unit extends JpaModel {
+	@Required
+	@NotNull
+	@Column(unique = true)
+	public String name;
 
-	public static Map<String, String> map() {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("L", "l");
-		map.put("KG", "kg");
-		return map;
+	public String toString() {
+		return name;
+	}
+
+	public static Unit findById(Long id) {
+		try {
+			if (id != null)
+				return JPA.em().find(Unit.class, id);
+		} catch (Exception e) {
+		}
+		return null;
+	}
+
+	public static Page page(int index) {
+		try {
+			int size = Play.application().configuration().getInt("page.size");
+			if (index < 1)
+				index = 1;
+			Integer rows = (Integer) JPA.em()
+					.createQuery("select count(*) from Unit").getSingleResult();
+			List<Unit> list = JPA.em()
+					.createQuery("from Unit order by name asc")
+					.setFirstResult((index - 1) * size).setMaxResults(size)
+					.getResultList();
+			if (rows != null || list != null)
+				return new Page(index, size, rows, list);
+		} catch (Exception e) {
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Map<String, String> options() {
+		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+		try {
+			List<Unit> units = JPA.em()
+					.createQuery("from Unit order by name").getResultList();
+			for (Unit unit : units)
+				map.put(unit.id.toString(), unit.toString());
+			return map;
+		} catch (Exception e) {
+			return map;
+		}
 	}
 }
