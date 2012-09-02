85db036980ffcbdda5351839d3ee3049ea7a0111
Sami <hostikka@gmail.com>
2012-09-02 03:01:13 +0300

    Added management view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,32 +9,11 @@ import views.html.*;
 
 public class Application extends Controller {
 
-	@Transactional
 	public static Result index() {
-		/*forms.dynamicforms.Field field = new forms.dynamicforms.Field();
-		field.help = "help";
-		field.name = "test";
-		field.type = models.dynamicforms.FieldType.TEXT;
-		field.isRequired = true;
-		field.isSigned = false;
-		//models.dynamicforms.Field f = new models.dynamicforms.Field(field);
-		forms.dynamicforms.Manage man = new forms.dynamicforms.Manage();
-		man.description = "desc";
-		man.isActive = false;
-		man.name = "test";
-		models.dynamicforms.Form form = new models.dynamicforms.Form(man);
-		form.addField(field);*/
-		//models.dynamicforms.Form form = models.dynamicforms.Form.findById(Long.parseLong("1"));
-		/*form.add(f);
-		form.save();*/
-		//models.dynamicforms.Field.findById(Long.parseLong("1")).delete();
-		
-		//models.dynamicforms.Form asd = models.dynamicforms.Form.findById(Long.parseLong("1"));
-		/*List<models.dynamicforms.Field> asd = models.dynamicforms.Field.findByForm(models.dynamicforms.Form.findById(Long.parseLong("1")));
-		if(asd != null)
-			System.out.println("\n\n"+asd.get(0).name+"\n\n");*/
-		
-		return ok(index.render("Your new application is ready."));
+		return ok(index.render());
 	}
 
+	public static Result management() {
+		return ok(management.render());
+	}
 }
