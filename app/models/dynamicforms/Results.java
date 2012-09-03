4fc3fe3bb26474c860643256a12c41030f922c2d
Sami <hostikka@gmail.com>
2012-09-03 10:45:04 +0300

    Trying to fix results

--- app/models/dynamicforms/Results.java
+++ app/models/dynamicforms/Results.java
@@ -7,10 +7,12 @@ import javax.validation.constraints.NotNull;
 
 import models.Batch;
 import models.helpers.JpaModel;
+import models.helpers.Page;
 
 import forms.Product;
 import forms.dynamicforms.Fieldset;
 
+import play.Play;
 import play.data.validation.Constraints.*;
 import play.db.jpa.*;
 
@@ -28,7 +30,7 @@ public class Results extends JpaModel {
 	@Required
 	@ManyToOne(cascade = CascadeType.ALL)
 	@NotNull
-	public Product product;
+	public models.Product product;
 
 	@Required
 	public Boolean isDone = false;
@@ -41,8 +43,8 @@ public class Results extends JpaModel {
 	public Results() {
 	}
 
-	public Results(Product product, List<Fieldset> values, Form form) {
-		this.product = product;
+	public Results(models.Product product2, List<Fieldset> values, Form form) {
+		this.product = product2;
 		this.updated = new Date();
 		this.form = form;
 		this.results = new ArrayList<Result>();
@@ -51,8 +53,13 @@ public class Results extends JpaModel {
 			results.add(new Result(value));
 	}
 
-	public static Results findById(int id) {
-		return JPA.em().find(Results.class, id);
+	public static Results findById(Long id) {
+		try {
+			if (id != null)
+				return JPA.em().find(Results.class, id);
+		} catch (Exception e) {
+		}
+		return null;
 	}
 
 	public static boolean getIsDone(Long batchId, Long formId) {
@@ -68,4 +75,23 @@ public class Results extends JpaModel {
 		}
 		return false;
 	}
+
+	public static Page page(int index) {
+		try {
+			int size = Play.application().configuration().getInt("page.size");
+			if (index < 1)
+				index = 1;
+			Long rows = (Long) JPA.em()
+					.createQuery("select count(*) from Results").getSingleResult();
+			List<Results> list = JPA.em()
+					.createQuery("from Results order by id asc")
+					.setFirstResult((index - 1) * size).setMaxResults(size)
+					.getResultList();
+			if (rows != null && list != null && !list.isEmpty())
+				return new Page(index, size, rows, list);
+		} catch (Exception e) {
+		}
+		return null;
+	}
+
 }
--- project/Build.scala
+++ project/Build.scala
@@ -10,11 +10,13 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
 		"org.hibernate" % "hibernate-entitymanager" % "3.6.10.Final",
 		"org.hibernate" % "hibernate-envers" % "3.6.10.Final",
-		"mysql" % "mysql-connector-java" % "5.1.18"
+		"mysql" % "mysql-connector-java" % "5.1.18",
+		"pdf" % "pdf_2.9.1" % "0.3"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-		ebeanEnabled := false
+		ebeanEnabled := false,
+		resolvers += Resolver.url("My GitHub Play Repository", url("http://www.joergviola.de/releases/"))(Resolver.ivyStylePatterns)
     )
 
 }
