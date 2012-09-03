0f4a1c3a57528850ee99af7198dcb4238aef6988
Sami <hostikka@gmail.com>
2012-09-03 06:18:16 +0300

    Added batch form helper

---
+++ app/forms/Batch.java
@@ -0,0 +1,36 @@
+package forms;
+
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import play.data.validation.Constraints.*;
+
+// TODO: Auto-generated Javadoc
+/**
+ * 
+ * @author Sami Hostikka <dev@01.fi>
+ */
+public class Batch {
+	@Required
+	@NotNull
+	public Product product;
+
+	@Required
+	@NotNull
+	public List<Long> ingredientSupplyId = new ArrayList<Long>();
+
+	@Required
+	@NotNull
+	public List<Double> amount = new ArrayList<Double>();
+
+	public Batch() {
+	}
+
+	public String validate() {
+		String result = null;
+		if(ingredientSupplyId.isEmpty() || amount.isEmpty())
+			result = "Raaka-aineita ei ole valittu";
+		return result;
+	}
+}
