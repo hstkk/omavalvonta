d61a60009d811f78804dce757765241edc5202c2
Sami <hostikka@gmail.com>
2012-11-30 00:02:46 +0200

    Added final product class

---
+++ app/models/FinalProduct.java
@@ -0,0 +1,82 @@
+package models;
+
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import models.helpers.Crud;
+import models.helpers.JpaModel;
+import models.helpers.Page;
+
+import org.hibernate.envers.Audited;
+
+import play.Play;
+import play.data.validation.Constraints.*;
+import play.db.jpa.*;
+
+/**
+ * 
+ * @author Sami Hostikka <dev@01.fi>
+ * 
+ */
+@Entity(name = "FinalProduct")
+@Audited
+public class FinalProduct extends JpaModel {
+
+	public final static Crud<FinalProduct, Long> crud = new Crud<>(
+			FinalProduct.class);
+
+	@Required
+	@NotNull
+	public Date date;
+
+	@Required
+	@NotNull
+	public Term destiny;
+
+	@Required
+	@NotNull
+	public Double amount;
+
+	@Required
+	@NotNull
+	public Term unit;
+
+	@Required
+	@NotNull
+	@Column(unique = true, updatable = false)
+	public Batch batch;
+
+	@Lob
+	public String comment;
+
+	public static FinalProduct findByBatch(Batch batch) {
+		try {
+			if (batch != null)
+				return (FinalProduct) JPA.em()
+						.createQuery("from FinalProduct " + "where batch = ?")
+						.setParameter(1, batch).getSingleResult();
+		} catch (Exception e) {
+		}
+		return null;
+	}
+
+	public static Page<FinalProduct> page(int index) {
+		try {
+			int size = Play.application().configuration().getInt("page.size");
+			if (index < 1)
+				index = 1;
+			Long rows = (Long) JPA.em()
+					.createQuery("select count(*) from FinalProduct")
+					.getSingleResult();
+			List<FinalProduct> list = JPA.em().createQuery("from FinalProduct")
+					.setFirstResult((index - 1) * size).setMaxResults(size)
+					.getResultList();
+			if (rows != null && list != null && !list.isEmpty())
+				return new Page(index, size, rows, list);
+		} catch (Exception e) {
+		}
+		return null;
+	}
+}
