caba5a9c1f5b9a66553ffdc13f810b79b7c3a349
Sami <hostikka@gmail.com>
2012-11-30 02:06:06 +0200

    Added form handling

--- app/controllers/FinalProducts.java
+++ app/controllers/FinalProducts.java
@@ -15,7 +15,7 @@ import views.html.*;
 
 public class FinalProducts extends Controller {
 
-	final static Form<FinalProduct> FORM = form(FinalProduct.class);
+	final static Form<forms.FinalProduct> FORM = form(forms.FinalProduct.class);
 
 	@Transactional(readOnly = true)
 	public static Result index() {
@@ -44,12 +44,20 @@ public class FinalProducts extends Controller {
 		Batch batch = Batch.findById(batchId);
 		if (batch == null)
 			return notFound();
-		Form<models.FinalProduct> filledForm = FORM.bindFromRequest();
+		Form<forms.FinalProduct> filledForm = FORM.bindFromRequest();
 		if (filledForm.field("action").value().equals("peruuta")) {
 			flash("warning", "Tallennus peruutettu!");
 			return redirect(routes.FinalProducts.read(batchId));
 		} else if (!filledForm.hasErrors()) {
-			if (FinalProduct.crud.createOrUpdate(filledForm.get())) {
+			FinalProduct finalProduct = FinalProduct.findByBatch(batch);
+			if (finalProduct == null)
+				finalProduct = new FinalProduct();
+			finalProduct.amount = filledForm.get().amount;
+			finalProduct.comment = filledForm.get().comment;
+			finalProduct.batch = batch;
+			finalProduct.unit = filledForm.get().unit;
+			finalProduct.destiny = filledForm.get().destiny;
+			if (FinalProduct.crud.createOrUpdate(finalProduct)) {
 				flash("success", "Tallennus onnistui!");
 				return redirect(routes.FinalProducts.read(batchId));
 			}
@@ -64,7 +72,7 @@ public class FinalProducts extends Controller {
 		if (batch == null)
 			return notFound();
 		FinalProduct result = FinalProduct.findByBatch(batch);
-		if(result != null)
+		if (result != null)
 			return ok(views.html.finalproducts.edit.render(FORM.fill(result)));
 		return ok(views.html.finalproducts.edit.render(FORM));
 	}
