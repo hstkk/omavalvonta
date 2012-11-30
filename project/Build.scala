db0225db054e724e8aa29919dbacad74011920f4
Sami <hostikka@gmail.com>
2012-11-30 06:47:29 +0200

    Downgradet play

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import play.Project._
+import PlayProject._
 
 object ApplicationBuild extends Build {
 
@@ -8,18 +8,12 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-		javaCore,
-		jdbc,
-		javaJdbc,
-		javaJpa,
-		filters,
 		"org.hibernate" % "hibernate-entitymanager" % "3.6.10.Final",
 		"org.hibernate" % "hibernate-envers" % "3.6.10.Final",
 		"mysql" % "mysql-connector-java" % "5.1.18"
     )
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
 		ebeanEnabled := false
     )
-
 }
