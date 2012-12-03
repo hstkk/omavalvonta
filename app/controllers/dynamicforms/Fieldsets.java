530fd24d471e7d38e78b9f0c9640dcebf4a2df62
Sami <hostikka@gmail.com>
2012-12-03 03:21:46 +0200

    now utiling crud controller

--- app/controllers/dynamicforms/Fieldsets.java
+++ app/controllers/dynamicforms/Fieldsets.java
@@ -18,19 +18,20 @@ import play.db.jpa.*;
 import models.dynamicforms.Field;
 import models.dynamicforms.Fieldset;
 
+import controllers.helpers.Crud;
+
 public class Fieldsets extends Controller {
 	final static models.helpers.Crud<Fieldset, Long> CRUD = new models.helpers.Crud<Fieldset, Long>(
 			Fieldset.class);
-	final static Form<Field> FORM = form(Field.class);
-	//final static Form<models.dynamicforms.Fieldset> FORM = play.data.Form.form(models.dynamicforms.Fieldset.class);
-	final static Map<String, String> TEMPLATES;
-	static {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Harry", "Potter");
-		TEMPLATES = Collections.unmodifiableMap(map);
-	}
-	//final static Template1 <models.helpers.Page<Fieldset>, Html> TEMPLATE =
-	//final static Html HTML = views.html.dynamicforms.fieldsets.page.ref;
-	//final static Template1 <models.helpers.Page<Fieldset>, Html> TEMPLATE =
-	Template1<models.helpers.Page<Fieldset>, Html> TEMPLATE = views.html.dynamicforms.fieldsets.page.ref();
+	final static Form<models.dynamicforms.Fieldset> FORM = form(models.dynamicforms.Fieldset.class);
+	/*
+	 * final static Map<String, String> TEMPLATES; static { Map<String, String>
+	 * map = new HashMap<String, String>(); map.put("Harry", "Potter");
+	 * TEMPLATES = Collections.unmodifiableMap(map); }
+	 */
+	final static Template1<models.helpers.Page<Fieldset>, Html> PAGETEMPLATE = views.html.dynamicforms.fieldsets.page
+			.ref();
+
+	public final static Crud<Fieldset> crud = new Crud<Fieldset>(
+			Fieldset.class, CRUD, FORM, PAGETEMPLATE);
 }
