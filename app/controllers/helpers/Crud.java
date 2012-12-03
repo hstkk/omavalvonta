0d6e6a9d36a5ca0865849460d49521de1e6b1f5a
Sami <hostikka@gmail.com>
2012-12-03 03:21:46 +0200

    now utiling crud controller

--- app/controllers/helpers/Crud.java
+++ app/controllers/helpers/Crud.java
@@ -3,6 +3,8 @@ package controllers.helpers;
 import models.dynamicforms.Fieldset;
 import play.*;
 import play.mvc.*;
+import play.api.templates.Html;
+import play.api.templates.Template1;
 import play.data.*;
 import static play.data.Form.*;
 import play.db.jpa.*;
@@ -12,11 +14,15 @@ public class Crud<T> extends Controller implements CrudInterface {
 	private final Class<T> clazz;
 	private final models.helpers.Crud<T, Long> CRUD;
 	private final Form<T> FORM;
+	private final Template1<models.helpers.Page<T>, Html> PAGETEMPLATE;
 
-	public Crud(Class<T> clazz, models.helpers.Crud<T, Long> CRUD, Form<T> FORM) {
+	public Crud(Class<T> clazz, models.helpers.Crud<T, Long> CRUD,
+			Form<T> FORM,
+			Template1<models.helpers.Page<T>, Html> PAGETEMPLATE) {
 		this.clazz = clazz;
 		this.CRUD = CRUD;
 		this.FORM = FORM;
+		this.PAGETEMPLATE = PAGETEMPLATE;
 	}
 
 	@Override
