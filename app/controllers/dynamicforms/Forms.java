3cd1f94b313cff15899995375e4e536f5c5ed147
Sami <hostikka@gmail.com>
2012-12-05 01:28:59 +0200

    Rewritten form controller

--- app/controllers/dynamicforms/Forms.java
+++ app/controllers/dynamicforms/Forms.java
@@ -1,34 +1,21 @@
 package controllers.dynamicforms;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 import play.api.templates.*;
 import play.data.*;
-import static play.data.Form.*;
-
-import play.*;
-import play.mvc.*;
-//import play.data.*;
-//import static play.data.Form.*;
-import play.db.jpa.*;
+import play.mvc.Controller;
 
 import models.dynamicforms.Form;
 
 import controllers.helpers.Crud;
 
 public class Forms extends Controller {
-	final static models.helpers.Crud<Form, Long> CRUD = new models.helpers.Crud<Form, Long>(
-			Form.class, "Form");
-	final static play.data.Form<Form> FORM = form(models.dynamicforms.Form.class);
 	final static Template1<models.helpers.Page<Form>, Html> PAGETEMPLATE = views.html.dynamicforms.forms.page
 			.ref();
 	final static Template1<play.data.Form<Form>, Html> CREATETEMPLATE = views.html.dynamicforms.forms.create
 			.ref();
 	final static Template2<Long, play.data.Form<Form>, Html> UPDATETEMPLATE = views.html.dynamicforms.forms.update
-			.ref();;
+			.ref();
 
-	public final static Crud<Form> crud = new Crud<Form>(Form.class, CRUD,
-			FORM, PAGETEMPLATE, CREATETEMPLATE, UPDATETEMPLATE);
+	public final static Crud<Form> crud = new Crud<Form>(Form.class,
+			PAGETEMPLATE, CREATETEMPLATE, UPDATETEMPLATE);
 }
