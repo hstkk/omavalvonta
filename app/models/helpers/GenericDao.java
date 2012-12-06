456ef176e68b8a98d95c5303b29849c53c499e99
Sami <hostikka@gmail.com>
2012-12-07 01:55:35 +0200

    Added new methods to dao interface and implemented count

--- app/models/helpers/GenericDao.java
+++ app/models/helpers/GenericDao.java
@@ -3,7 +3,15 @@ package models.helpers;
 import java.io.Serializable;
 
 public interface GenericDao<T, ID extends Serializable> {
+	boolean exists(T t);
+
 	boolean create(T t);
+
+	int count();
+
+	//List<T> findAll();
+
+	//List<T> findAllByProperty(String property, Object value);
 
 	T findById(ID id);
 
