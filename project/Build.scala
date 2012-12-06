a6addc667f1941d9cb632321696654bf284cf27c
Sami <hostikka@gmail.com>
2012-12-07 00:08:20 +0200

    Removed beanutils library

--- project/Build.scala
+++ project/Build.scala
@@ -10,8 +10,7 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
 		"org.hibernate" % "hibernate-entitymanager" % "3.6.10.Final",
 		"org.hibernate" % "hibernate-envers" % "3.6.10.Final",
-		"mysql" % "mysql-connector-java" % "5.1.18",
-		"commons-beanutils" % "commons-beanutils" % "1.8.3"
+		"mysql" % "mysql-connector-java" % "5.1.18"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
