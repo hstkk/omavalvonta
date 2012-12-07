4db27da0ecf5555617c3ef48d6f1b61c21ea528e
Sami <hostikka@gmail.com>
2012-12-08 01:12:13 +0200

    Rewritten variable names on crud controller

--- app/controllers/helpers/Crud.java
+++ app/controllers/helpers/Crud.java
@@ -1,9 +1,12 @@
 package controllers.helpers;
 
 import models.helpers.Model;
+import models.helpers.Page;
+
 import play.*;
 import play.i18n.Messages;
 import play.mvc.*;
+import play.api.mvc.Call;
 import play.api.templates.*;
 import play.data.*;
 import static play.data.Form.*;
@@ -13,20 +16,27 @@ public class Crud<T extends Model> extends Controller implements CrudInterface {
 	private final Class<T> clazz;
 	private final Form<T> FORM;
 	private final models.helpers.Crud<T, Long> CRUD;
-	private final Template1<models.helpers.Page<T>, Html> PAGETEMPLATE;
-	private final Template1<Form<T>, Html> CREATETEMPLATE;
-	private final Template2<Long, Form<T>, Html> UPDATETEMPLATE;
+	private final Template2<Long, Form<T>, Html> EDIT;
+	private final Template1<Form<T>, Html> FRESH;
+	private final Template1<Page<T>, Html> PAGE;
+	private final Call REDIRECT;
+	private final Template1<T, Html> SHOW;
 
-	public Crud(Class<T> clazz,
-			Template1<models.helpers.Page<T>, Html> PAGETEMPLATE,
-			Template1<Form<T>, Html> CREATETEMPLATE,
-			Template2<Long, Form<T>, Html> UPDATETEMPLATE) {
+	Result notFound = notFound(views.html.error.render(
+			Messages.get("http.404"), Messages.get("http.404.description")));
+
+	public Crud(Class<T> clazz, models.helpers.Crud<T, Long> CRUD,
+			Template2<Long, Form<T>, Html> EDIT,
+			Template1<Form<T>, Html> FRESH, Template1<Page<T>, Html> PAGE,
+			Call REDIRECT, Template1<T, Html> SHOW) {
 		this.clazz = clazz;
-		this.PAGETEMPLATE = PAGETEMPLATE;
-		this.CREATETEMPLATE = CREATETEMPLATE;
-		this.UPDATETEMPLATE = UPDATETEMPLATE;
 		this.FORM = form(this.clazz);
-		this.CRUD = new models.helpers.Crud<T, Long>(this.clazz);
+		this.CRUD = CRUD;
+		this.EDIT = EDIT;
+		this.FRESH = FRESH;
+		this.PAGE = PAGE;
+		this.REDIRECT = REDIRECT;
+		this.SHOW = SHOW;
 	}
 
 	@Override
@@ -35,18 +45,16 @@ public class Crud<T extends Model> extends Controller implements CrudInterface {
 		Form<T> filledForm = FORM.bindFromRequest();
 		if (filledForm.field("action").value().equals("peruuta")) {
 			flash("warning", Messages.get("crud.cancel"));
-			// return redirect(page(1));
-			return page(1);
+			return redirect(REDIRECT);
 		} else if (!filledForm.hasErrors()) {
 			T t = filledForm.get();
 			if (CRUD.create(t)) {
 				flash("success", Messages.get("crud.success"));
-				// return redirect(routes.dynamicforms.Fieldsets.crud.page(1));
-				return page(1);
+				return redirect(REDIRECT);
 			}
 		}
 		flash("error", Messages.get("crud.fail"));
-		return badRequest(CREATETEMPLATE.render(filledForm));
+		return badRequest(FRESH.render(filledForm));
 	}
 
 	@Override
@@ -56,25 +64,28 @@ public class Crud<T extends Model> extends Controller implements CrudInterface {
 		if (t == null)
 			return notFound();
 		Form<T> filledForm = FORM.fill(t);
-		return ok(UPDATETEMPLATE.render(id, filledForm));
+		return ok(EDIT.render(id, filledForm));
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Result fresh() {
-		return ok(CREATETEMPLATE.render(FORM));
+		return ok(FRESH.render(FORM));
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Result page(int index) {
-		return ok(PAGETEMPLATE.render(CRUD.page(index)));
+		return ok(PAGE.render(CRUD.page(index)));
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Result show(Long id) {
-		return TODO;
+		T t = CRUD.findById(id);
+		if (t == null)
+			return notFound();
+		return ok(SHOW.render(t));
 	}
 
 	@Override
@@ -85,18 +96,16 @@ public class Crud<T extends Model> extends Controller implements CrudInterface {
 		Form<T> filledForm = FORM.bindFromRequest();
 		if (filledForm.field("action").value().equals("peruuta")) {
 			flash("warning", Messages.get("crud.cancel"));
-			// return redirect(page(1));
-			return page(1);
+			return redirect(REDIRECT);
 		} else if (!filledForm.hasErrors()) {
 			T fresh = filledForm.get();
 			fresh.id = id;
 			if (CRUD.update(fresh)) {
 				flash("success", Messages.get("crud.success"));
-				// return redirect(routes.dynamicforms.Fieldsets.crud.page(1));
-				return page(1);
+				return redirect(REDIRECT);
 			}
 		}
 		flash("error", Messages.get("crud.fail"));
-		return badRequest(UPDATETEMPLATE.render(id, filledForm));
+		return badRequest(EDIT.render(id, filledForm));
 	}
 }
