b565bdbe9bf40a2822f5588eac2434dc38f84cb9
Sami <hostikka@gmail.com>
2012-12-08 00:24:01 +0200

    Added show method and ordered alphabetical

--- app/controllers/helpers/CrudInterface.java
+++ app/controllers/helpers/CrudInterface.java
@@ -3,13 +3,15 @@ package controllers.helpers;
 import play.mvc.Result;
 
 public interface CrudInterface {
-	Result page(int index);
-
-	Result fresh();
-
 	Result create();
 
 	Result edit(Long id);
+
+	Result fresh();
+
+	Result page(int index);
+
+	Result show(Long id);
 
 	Result update(Long id);
 }
