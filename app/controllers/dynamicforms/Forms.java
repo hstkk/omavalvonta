e52e842b6f7eecfc4a59eea7d40a0c4bda3643cb
Sami <hostikka@gmail.com>
2012-12-08 01:42:26 +0200

    Implemented new crud controller

--- app/controllers/dynamicforms/Forms.java
+++ app/controllers/dynamicforms/Forms.java
@@ -1,21 +1,13 @@
 package controllers.dynamicforms;
 
-import play.api.templates.*;
-import play.data.*;
-import play.mvc.Controller;
-
 import models.dynamicforms.Form;
-
 import controllers.helpers.Crud;
+import play.mvc.Controller;
+import views.html.dynamicforms.forms.*;
 
 public class Forms extends Controller {
-	final static Template1<models.helpers.Page<Form>, Html> PAGETEMPLATE = views.html.dynamicforms.forms.page
-			.ref();
-	final static Template1<play.data.Form<Form>, Html> CREATETEMPLATE = views.html.dynamicforms.forms.create
-			.ref();
-	final static Template2<Long, play.data.Form<Form>, Html> UPDATETEMPLATE = views.html.dynamicforms.forms.update
-			.ref();
-
-	public final static Crud<Form> crud = new Crud<Form>(Form.class,
-			PAGETEMPLATE, CREATETEMPLATE, UPDATETEMPLATE);
+	public final static Crud<Form> crud = new Crud<Form>(Form.crud,
+			form(Form.class),
+			update.ref(), create.ref(), page.ref(), null,
+			routes.Forms.crud.page(0));
 }
