8662c49a3b488f06638a95a98d358a3c03d8947a
Sami <hostikka@gmail.com>
2012-12-09 09:54:15 +0200

    Finetuned crud controllers

--- app/controllers/dynamicforms/Fieldsets.java
+++ app/controllers/dynamicforms/Fieldsets.java
@@ -2,11 +2,16 @@ package controllers.dynamicforms;
 
 import models.dynamicforms.Fieldset;
 import controllers.helpers.Crud;
+import static play.data.Form.*;
 import play.mvc.Controller;
 import views.html.dynamicforms.fieldsets.*;
 
 public class Fieldsets extends Controller {
 	public final static Crud<Fieldset> crud = new Crud<Fieldset>(Fieldset.crud,
-			form(Fieldset.class), update.ref(), create.ref(), page.ref(), null,
+			form(Fieldset.class),
+			update.ref(),
+			create.ref(),
+			page.ref(),
+			null,
 			routes.Fieldsets.crud.page(1, "", ""));
 }
--- app/controllers/dynamicforms/Forms.java
+++ app/controllers/dynamicforms/Forms.java
@@ -2,12 +2,16 @@ package controllers.dynamicforms;
 
 import models.dynamicforms.Form;
 import controllers.helpers.Crud;
+import static play.data.Form.*;
 import play.mvc.Controller;
 import views.html.dynamicforms.forms.*;
 
 public class Forms extends Controller {
 	public final static Crud<Form> crud = new Crud<Form>(Form.crud,
 			form(Form.class),
-			update.ref(), create.ref(), page.ref(), null,
+			update.ref(),
+			create.ref(),
+			page.ref(),
+			null,
 			routes.Forms.crud.page(1, "", ""));
 }
