45418bb7c407d82d772316354944aa2ed3911e0d
Sami <hostikka@gmail.com>
2012-12-20 07:03:54 +0200

    Fixed page usage

--- app/models/IngredientSupply.java
+++ app/models/IngredientSupply.java
@@ -170,7 +170,7 @@ public class IngredientSupply extends JpaModel {
 			int size = Play.application().configuration().getInt("page.size");
 			if (index < 1)
 				index = 1;
-			int rows = (int) JPA.em()
+			long rows = (long) JPA.em()
 					.createQuery("select count(*) from IngredientSupply")
 					.getSingleResult();
 			List<IngredientSupply> list = JPA
@@ -183,6 +183,6 @@ public class IngredientSupply extends JpaModel {
 				return new Page(index, size, rows, list);
 		} catch (Exception e) {
 		}
-		return null;
+		return new Page(index, 0, 0, null);
 	}
 }
