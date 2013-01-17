558b578a52bd6934f38790cb48855a70043aa5e5
Sami <hostikka@gmail.com>
2013-01-17 11:22:02 +0200

    Migrated terms to crud

--- app/controllers/Terms.java
+++ app/controllers/Terms.java
@@ -1,86 +1,17 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-import play.data.*;
-import static play.data.Form.*;
-import play.db.jpa.*;
-
 import models.Term;
-import models.TermCategory;
-import models.helpers.Page;
-
-import views.html.*;
+import controllers.helpers.Crud;
+import static play.data.Form.*;
+import play.mvc.Controller;
+import views.html.terms.*;
 
 public class Terms extends Controller{
-
-	final static Form<Term> FORM = form(Term.class);
-
-	public static Result create() {
-		return ok(views.html.terms.manage.render(FORM));
-	}
-
-	/*@Transactional(readOnly = true)
-	public static Result read(Long id) {
-		Term term = Term.crud.read(id);
-		if (term == null)
-			return notFound();
-		return ok(views.html.terms.manage.render(FORM.fill(new forms.Term(term))));
-	}
-
-	@Transactional
-	public static Result update(Long id) {
-		Term term = Term.crud.read(id);
-		if (term == null)
-			return notFound();
-		Form<forms.Term> filledForm = FORM.bindFromRequest();
-		if (filledForm.field("action").value().equals("peruuta")) {
-			flash("warning", "Tallennus peruutettu!");
-			return redirect(routes.Terms.index());
-		} else if (!filledForm.hasErrors()) {
-			term.set(filledForm.get());
-			if (Term.crud.update(term)) {
-				flash("success", "Tallennus onnistui!");
-				return redirect(routes.Terms.index());
-			}
-		}
-		flash("error", "Tallennus epäonnistui!");
-		return badRequest(views.html.terms.manage.render(filledForm));
-	}
-
-	@Transactional
-	public static Result delete(Long id) {
-		Term term = Term.crud.read(id);
-		if (term == null)
-			return notFound();
-		Term.crud.delete(term);
-		return redirect(routes.Terms.index());
-	}*/
-
-	@Transactional
-	public static Result save() {
-		Form<Term> filledForm = FORM.bindFromRequest();
-		if (filledForm.field("action").value().equals("peruuta")) {
-			flash("warning", "Tallennus peruutettu!");
-			return redirect(routes.Terms.index());
-		} else if (!filledForm.hasErrors()) {
-			if (Term.crud.create(filledForm.get())) {
-				flash("success", "Tallennus onnistui!");
-				return redirect(routes.Terms.index());
-			}
-		}
-		flash("error", "Tallennus epäonnistui!");
-		return badRequest(views.html.terms.manage.render(filledForm));
-	}
-
-	@Transactional(readOnly = true)
-	public static Result index() {
-		return page(1);
-	}
-
-	@Transactional(readOnly = true)
-	public static Result page(int index) {
-		Page<Term> page = Term.page(index);
-		return ok(views.html.terms.page.render(page));
-	}
+	public final static Crud<Term> crud = new Crud<Term>(Term.crud,
+			form(Term.class),
+			update.ref(),
+			create.ref(),
+			page.ref(),
+			null,
+			routes.Terms.crud.page(1, "", ""));
 }
--- app/models/Term.java
+++ app/models/Term.java
@@ -21,11 +21,12 @@ import play.db.jpa.*;
  * @author Sami Hostikka <dev@01.fi>
  * 
  */
+//TODO Term
 @Entity(name = "Term")
 @Audited
 public class Term extends Model {
 
-	public final static Crud<Term, Long> crud = new Crud<>(Term.class);
+	public final static Crud<Term, Long> crud = new Crud<Term, Long>(Term.class);
 
 	@Column(name = "name")
 	//@Required
@@ -53,22 +54,6 @@ public class Term extends Model {
 
 	public String toString() {
 		return name;
-	}
-
-	public static Page<Term> page(int index) {
-		try {
-			int size = Play.application().configuration().getInt("page.size");
-			if (index < 1)
-				index = 1;
-			long rows = (long) JPA.em()
-					.createQuery("select count(*) from Term").getSingleResult();
-			List<Term> list = JPA.em().createQuery("from Term order by name")
-					.setFirstResult((index - 1) * size).setMaxResults(size)
-					.getResultList();
-				return new Page(index, size, rows, list);
-		} catch (Exception e) {
-		}
-		return new Page(index, 0, 0, null);
 	}
 
 	@SuppressWarnings("unchecked")
