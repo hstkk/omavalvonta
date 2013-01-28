e1baeb2ce08f02999e2b23c0f8bb2f871542c740
Sami <hostikka@gmail.com>
2013-01-28 14:00:08 +0200

    Added secured crud to management controllers

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,11 @@
 package controllers;
 
-import java.util.*;
-
+import controllers.shib.Secured;
 import play.*;
 import play.mvc.*;
 import play.data.*;
 import static play.data.Form.*;
 import play.db.jpa.*;
-
 import views.html.*;
 
 public class Application extends Controller {
@@ -16,6 +14,7 @@ public class Application extends Controller {
 		return ok(index.render());
 	}
 
+	@Security.Authenticated(Secured.class)
 	public static Result management() {
 		return ok(management.render());
 	}
--- app/controllers/dynamicforms/Forms.java
+++ app/controllers/dynamicforms/Forms.java
@@ -1,12 +1,12 @@
 package controllers.dynamicforms;
 
 import models.dynamicforms.Form;
-import controllers.helpers.Crud;
+import controllers.helpers.SecuredCrud;
 import play.mvc.Controller;
 import views.html.dynamicforms.forms.*;
 
 public class Forms extends Controller {
-	public final static Crud<Form> crud = new Crud<Form>(Form.crud,
+	public final static SecuredCrud<Form> crud = new SecuredCrud<Form>(Form.crud,
 			form(Form.class),
 			update.ref(),
 			create.ref(),
