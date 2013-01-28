298914376d32a7d9f504b4f96ffcaa1a26547cec
Sami <hostikka@gmail.com>
2013-01-28 13:42:35 +0200

    Added initial shibboleth implementation

---
+++ app/controllers/shib/Shibboleth.java
@@ -0,0 +1,23 @@
+package controllers.shib;
+
+import play.*;
+import play.mvc.*;
+import play.db.jpa.*;
+
+public class Shibboleth {
+	// 2.
+	@Transactional
+	public static Result authenticate() {
+		return null;
+	}
+
+	// 1.
+	public static Result login() {
+		return null;
+	}
+
+	// 3.
+	public static Result logout() {
+		return null;
+	}
+}
---
+++ app/utils/ShibbolethDefaults.java
@@ -0,0 +1,5 @@
+package utils;
+
+public class ShibbolethDefaults {
+	public static final String ROLE = "stuff";
+}
