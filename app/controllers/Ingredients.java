dac45c9e261c2acfd8cc48fa555f8817b338eb4d
Sami <hostikka@gmail.com>
2013-02-06 21:27:45 +0200

    Migrated to play 2.1, it doesn't work

--- app/controllers/Ingredients.java
+++ app/controllers/Ingredients.java
@@ -7,11 +7,25 @@ import views.html.ingredients.*;
 
 public class Ingredients extends Controller {
 
-	public final static SecuredCrud<Ingredient> crud = new SecuredCrud<Ingredient>(Ingredient.crud,
+	/*public final static SecuredCrud<Ingredient> crud = new SecuredCrud<Ingredient>(Ingredient.crud,
 			form(Ingredient.class),
 			update.ref(),
 			create.ref(),
 			page.ref(),
 			null,
-			routes.Ingredients.crud.page(1));
+			routes.Ingredients.crud.page(1));*/
+
+	static SecuredCrud<Ingredient> crud;
+
+	public static SecuredCrud<Ingredient> getCrud() {
+		if (crud == null)
+			crud = new SecuredCrud<Ingredient>(Ingredient.crud,
+					form(Ingredient.class),
+					update.ref(),
+					create.ref(),
+					page.ref(),
+					null,
+					routes.Ingredients.crud.page(1));
+		return crud;
+	}
 }
--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -8,13 +8,17 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
+		javaCore,
+		javaJdbc,
+		javaJpa,
+		filters,
 		"org.hibernate" % "hibernate-entitymanager" % "3.6.10.Final",
 		"org.hibernate" % "hibernate-envers" % "3.6.10.Final",
 		"mysql" % "mysql-connector-java" % "5.1.22",
 		"org.hibernate" % "hibernate-jpamodelgen" % "1.2.0.Final"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
 		ebeanEnabled := false,
 		javacOptions ++= Seq("-s", "metamodel")
     )
