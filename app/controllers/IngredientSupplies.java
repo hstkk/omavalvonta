17345169ce2e88cfd9a3c0d86358b08facea5fbd
Sami <hostikka@gmail.com>
2013-02-11 01:02:45 +0200

    Implemented crud on ingredient supplies

--- app/controllers/IngredientSupplies.java
+++ app/controllers/IngredientSupplies.java
@@ -6,22 +6,12 @@ import play.mvc.Controller;
 import views.html.ingredientsupplies.*;
 
 public class IngredientSupplies extends Controller {
-	public final static SecuredCrud<IngredientSupply> crud = new SecuredCrud<IngredientSupply>(IngredientSupply.crud,
+	public final static SecuredCrud<IngredientSupply> crud = new SecuredCrud<IngredientSupply>(
+			IngredientSupply.crud,
 			form(IngredientSupply.class),
 			null,
 			create.ref(),
 			page.ref(),
 			show.ref(),
 			routes.Terms.crud.page(1));
-
-	/*@Transactional(readOnly = true)
-	public static Result read(Long ingredientId) {
-		IngredientSupply ingredientSupply = IngredientSupply
-				.findById(ingredientId);
-		if (ingredientSupply == null)
-			return notFound();
-		List<Batch> batches = Batch.findByIngredientSupply(ingredientId);
-		return ok(views.html.ingredientsupplies.read.render(ingredientSupply,
-				batches));
-	}*/
 }
