17973ea01c21a2caf80d6a7449ee1969f66f5238
Sami <hostikka@gmail.com>
2013-02-10 22:59:00 +0200

    Added new helper to global and cleaned code

--- app/Global.java
+++ app/Global.java
@@ -1,41 +1,34 @@
-import play.*;
-import play.i18n.Lang;
-import play.i18n.Messages;
-import play.mvc.*;
-import play.mvc.Http.*;
-import static play.mvc.Results.*;
+import static play.mvc.Results.badRequest;
+import static play.mvc.Results.internalServerError;
+import static play.mvc.Results.notFound;
+import play.GlobalSettings;
+import play.mvc.Http.RequestHeader;
+import play.mvc.Result;
+import utils.Helper;
 
 /**
  * @author Sami Hostikka
  */
 public class Global extends GlobalSettings {
 
-	protected static Lang lang;
-
-	private static Lang getLang() {
-		if (lang == null)
-			lang = Lang.preferred(Lang.availables());
-		return lang;
+	@Override
+	public Result onBadRequest(RequestHeader request, String error) {
+		String title = Helper.getMessage("http.400");
+		String description = Helper.getMessage("http.400.description");
+		return badRequest(views.html.error.render(title, description));
 	}
 
 	@Override
 	public Result onError(RequestHeader request, Throwable t) {
-		String title = Messages.get(getLang(), "http.500");
-		String description = Messages.get(getLang(), "http.500.description");
+		String title = Helper.getMessage("http.500");
+		String description = Helper.getMessage("http.500.description");
 		return internalServerError(views.html.error.render(title, description));
 	}
 
 	@Override
 	public Result onHandlerNotFound(RequestHeader request) {
-		String title = Messages.get(getLang(), "http.404");
-		String description = Messages.get(getLang(), "http.404.description");
+		String title = Helper.getMessage("http.404");
+		String description = Helper.getMessage("http.404.description");
 		return notFound(views.html.error.render(title, description));
-	}
-
-	@Override
-	public Result onBadRequest(RequestHeader request, String error) {
-		String title = Messages.get(getLang(), "http.400");
-		String description = Messages.get(getLang(), "http.400.description");
-		return badRequest(views.html.error.render(title, description));
 	}
 }
