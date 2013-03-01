e53ffd76558260338eedbb4083915350598b45ed
Sami <hostikka@gmail.com>
2013-03-01 14:35:45 +0200

    Finetune imports

--- app/models/dynamicforms/Form.java
+++ app/models/dynamicforms/Form.java
@@ -1,15 +1,11 @@
 package models.dynamicforms;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.constraints.NotNull;
 
 import models.helpers.Crud;
 import models.helpers.UserModel;
 
 import org.hibernate.envers.Audited;
-
-import play.data.validation.Constraints.Required;
 
 //TODO
 @Entity
