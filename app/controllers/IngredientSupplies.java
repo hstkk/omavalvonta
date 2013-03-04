f078db22147a39fe1464dbd0f98bbd12297b972a
Sami <hostikka@gmail.com>
2013-03-04 10:39:15 +0200

    Fix ingredient supply controller and findByTerm query

--- app/controllers/IngredientSupplies.java
+++ app/controllers/IngredientSupplies.java
@@ -1,12 +1,14 @@
 package controllers;
 
 import models.IngredientSupply;
-import controllers.helpers.SecuredCrud;
 import play.mvc.Controller;
-import views.html.ingredientsupplies.*;
+import views.html.ingredientsupplies.create;
+import views.html.ingredientsupplies.page;
+import views.html.ingredientsupplies.show;
+import controllers.helpers.UserCrud;
 
 public class IngredientSupplies extends Controller {
-	public final static SecuredCrud<IngredientSupply> crud = new SecuredCrud<IngredientSupply>(
+	public final static UserCrud<IngredientSupply> crud = new UserCrud<IngredientSupply>(
 			IngredientSupply.crud,
 			form(IngredientSupply.class),
 			null,
