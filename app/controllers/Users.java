3be6f4d8f81c0ee7b5825c13166d33c2dd819e83
Sami <hostikka@gmail.com>
2013-03-19 10:16:56 +0200

    Add controller for user

---
+++ app/controllers/Users.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import models.User;
+import play.mvc.Call;
+import play.mvc.Controller;
+import views.html.users.*;
+import controllers.helpers.Crud;
+
+public class Users extends Controller {
+	public final static Crud<User> crud = new Crud<User>(
+			User.crud,
+			null,
+			new Router(),
+			null,
+			page.ref(),
+			show.ref(),
+			null
+		);
+
+	public static class Router extends controllers.helpers.Router {
+		@Override
+		public play.mvc.Call page() {
+			return controllers.routes.Users.crud.page(1);
+		}
+
+		@Override
+		public play.mvc.Call show(Long id) {
+			return controllers.routes.Users.crud.show(id);
+		}
+	}
+}
