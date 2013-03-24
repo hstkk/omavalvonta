bc72f3fa6de5548e85bdad708cebb3552141e9d8
Sami <hostikka@gmail.com>
2013-03-25 00:23:40 +0200

    Finetune joins

--- app/models/Batch.java
+++ app/models/Batch.java
@@ -1,10 +1,11 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
-
+import javax.persistence.OneToMany;
 import models.helpers.Crud;
 import models.helpers.UserModel;
-
 import org.hibernate.envers.Audited;
 
 //TODO
@@ -12,4 +13,7 @@ import org.hibernate.envers.Audited;
 @Audited
 public class Batch extends UserModel {
 	public final static Crud<Batch, Long> crud = new Crud<Batch, Long>(Batch.class);
+
+	@OneToMany(mappedBy = "batch")
+	public List<IngredientSupplyBatch> ingredientSupplies = new ArrayList<IngredientSupplyBatch>();
 }
