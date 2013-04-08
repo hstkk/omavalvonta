ffcb1780a44c586d1c3e2c0ef88421a145cb40fe
Sami <hostikka@gmail.com>
2013-04-08 08:24:23 +0300

    Finetune ingredient supply batch models binding and validation

--- app/models/IngredientSupplyBatch.java
+++ app/models/IngredientSupplyBatch.java
@@ -6,6 +6,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import models.Batch.All;
+import models.Batch.Step2;
 import models.helpers.Model;
 import org.hibernate.envers.Audited;
 import play.data.validation.Constraints.Min;
@@ -21,15 +24,33 @@ public class IngredientSupplyBatch extends Model {
 	@JoinColumn(name = "ingredientSupply_id", updatable = false)
 	public IngredientSupply ingredientSupply;
 
-	@Required
-	@NotNull
+	@Required(groups = { All.class })
+	@NotNull(groups = { All.class })
 	@ManyToOne
 	@JoinColumn(name = "batch_id", updatable = false)
 	public Batch batch;
 
 	@Required
-	@NotNull
+	@NotNull(groups = { All.class })
 	@Min(0)
 	@Column(updatable = false)
 	public Double amount;
+
+	public boolean bind(Batch batch) {
+		this.batch = batch;
+
+		if (this.amount < 0)
+			return false;
+
+		this.ingredientSupply = IngredientSupply.dao
+				.findById(this.ingredientSupply.id);
+		if (this.ingredientSupply == null)
+			return false;
+
+		boolean success = this.ingredientSupply.isAvailable(this.amount);
+		if (success)
+			this.ingredientSupply.use(this.amount);
+
+		return success;
+	}
 }
