c30852d0b45914199d11c67803d26f15d30c6fca
Sami <hostikka@gmail.com>
2013-04-08 22:41:29 +0300

    Added helpers to shib session

--- app/controllers/shib/Session.java
+++ app/controllers/shib/Session.java
@@ -1,18 +1,32 @@
 package controllers.shib;
 
+import models.User;
 import play.mvc.Action;
 import play.mvc.Http.Context;
 import play.mvc.Result;
 import utils.ShibbolethHelper;
 
-public class SessionTimeout extends Action.Simple {
+public class Session extends Action.Simple {
 	@Override
 	public Result call(Context ctx) throws Throwable {
 		Context.current.set(ctx);
+
+		// timeout
 		if (ShibbolethHelper.isSession(ctx)
 				&& !ctx.request().method().equalsIgnoreCase("POST")
-				&& !ShibbolethHelper.isSessionValid(ctx))
+				&& !ShibbolethHelper.isSessionValid(ctx)) {
+			System.out.println("Timeout");
 			return Shibboleth.logout();
+		}
+
 		return delegate.call(ctx);
+	}
+
+	public static User user() {
+		return ShibbolethHelper.getSessionUser(Context.current());
+	}
+
+	public static String username() {
+		return ShibbolethHelper.getSessionUserEmail(Context.current());
 	}
 }
