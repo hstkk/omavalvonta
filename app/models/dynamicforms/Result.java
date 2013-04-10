5ee6fa454f08b10bc4e6b34923853b1f5646c1c8
Sami <hostikka@gmail.com>
2013-04-11 00:25:31 +0300

    Add result and results models

---
+++ app/models/dynamicforms/Result.java
@@ -0,0 +1,67 @@
+package models.dynamicforms;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.Audited;
+
+import play.data.validation.Constraints.Required;
+import play.db.jpa.JPA;
+
+import models.Term;
+import models.helpers.Dao;
+import models.helpers.UserModel;
+
+@Entity
+@Audited
+public class Result extends UserModel {
+	public final static Dao<Result, Long> dao = new Dao<Result, Long>(
+			Result.class);
+
+	@Required
+	@NotNull
+	@Valid
+	@ManyToOne
+	@JoinColumn(name = "id", insertable = false, updatable = false, nullable = false)
+	public Results results;
+
+	@Lob
+	public String valueString;
+
+	public Integer valueInt;
+
+	public Double valueDouble;
+
+	public Boolean valueBoolean;
+
+	public Date valueDate;
+
+	@Lob
+	public String comment;
+
+	@Valid
+	@ManyToOne(cascade = CascadeType.ALL)
+	public Term reason;
+
+	public List<Result> getHistory() {
+		List<Result> history = new ArrayList<Result>();
+		AuditReader auditReader = AuditReaderFactory.get(JPA.em());
+		List<Number> revisions = auditReader.getRevisions(this.getClass(), id);
+		for (Number revision : revisions) {
+			Result result = auditReader.find(this.getClass(), id, revision);
+			history.add(result);
+		}
+		return history;
+	}
+}
---
+++ app/models/dynamicforms/Results.java
@@ -0,0 +1,42 @@
+package models.dynamicforms;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.hibernate.envers.Audited;
+import play.data.validation.Constraints.Required;
+import models.Batch;
+import models.helpers.Dao;
+import models.helpers.Model;
+
+@Entity
+@Audited
+public class Results extends Model {
+	public final static Dao<Results, Long> dao = new Dao<Results, Long>(
+			Results.class);
+
+	@Required
+	@ManyToOne(cascade = CascadeType.ALL)
+	@NotNull
+	@Valid
+	public Form form;
+
+	@Required
+	@ManyToMany(cascade = CascadeType.ALL)
+	@NotNull
+	@Valid
+	public List<Batch> baches = new ArrayList<Batch>();
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "results_id")
+	@Valid
+	@NotNull
+	public List<Result> results = new ArrayList<Result>();
+}
