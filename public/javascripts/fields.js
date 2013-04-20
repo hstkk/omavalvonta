98cfaaf0042df3d769f43aa4ce0c7e54ce3936bb
Sami <hostikka@gmail.com>
2013-04-21 01:08:25 +0300

    Add sortable field management

--- public/javascripts/fields.js
+++ public/javascripts/fields.js
@@ -1,17 +1,38 @@
 $(function() {
 	var sortable = $('#sortable');
 	sortable.sortable({
-		placeholder: "ui-state-highlight"
+		axis: 'y',
+		cursor: 'move',
+		forcePlaceholderSize: true,
+		handle: 'h3',
+		update: renumber
 	});
 	sortable.disableSelection();
 
-	$('.addField').live('click', function(e) {
+	sortable.accordion({
+		collapsible: true,
+		header: '> fieldset > h3'
+	});
+
+	// add form fields
+	$('.addField').on('click', function() {
 		var template = $('.field_template');
 		sortable.append(template.html());
+		sortable.accordion('refresh');
+		sortable.accordion('option', 'active', -1);
 		renumber();
 	});
 
-var renumber = function(){
+	// renumber form fields
+	var renumber = function(){
+	
+	};
 
-};
+	// update field header
+	sortable.on('change', '.field_name', function() {
+		var that = $(this),
+		    value = that.val(),
+		    header = that.closest('fieldset').find('h3 .field_header');
+		header.text(value);
+	});
 });
