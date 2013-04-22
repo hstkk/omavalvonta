943800d4a03e25a497a8837a577f1f3b4728ee31
Sami <hostikka@gmail.com>
2013-04-22 07:30:21 +0300

    Fix fieldsets onetomany fields

--- app/models/dynamicforms/Field.java
+++ app/models/dynamicforms/Field.java
@@ -1,32 +1,20 @@
 package models.dynamicforms;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.PostLoad;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
 import javax.persistence.Transient;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-
-import models.User;
 import models.helpers.Dao;
-import models.helpers.UserModel;
+import models.helpers.Model;
 import org.hibernate.envers.Audited;
 import play.data.validation.Constraints.*;
-import play.i18n.Messages;
 
 @Entity
 @Audited
-public class Field extends UserModel {
+public class Field extends Model {
 	public final static Dao<Field, Long> dao = new Dao<Field, Long>(Field.class);
 
 	@Required
@@ -40,19 +28,16 @@ public class Field extends UserModel {
 	@NotNull
 	public boolean isRequired;
 
-	@Required
-	@NotNull
-	@Valid
 	@ManyToOne
-	@JoinColumn(name = "id", insertable = false, updatable = false, nullable = false)
+	@JoinColumn(name = "fieldset_id", insertable = false, updatable = false, nullable = false)
 	public Fieldset fieldset;
-
-	@Transient
-	public FieldType fieldTypeEnum;
 
 	@Required
 	@NotNull
 	public Integer fieldType;
+
+	@Transient
+	public FieldType fieldTypeEnum;
 
 	@Required
 	@NotNull
@@ -68,51 +53,9 @@ public class Field extends UserModel {
 		return name;
 	}
 
-	@PrePersist
-	@PreUpdate
-	private void onPre() {
-		if (fieldTypeEnum != null)
-			fieldType = fieldTypeEnum.getValue();
-	}
-
 	@PostLoad
-	private void intToEnum() {
-		if (fieldType != null)
+	private void onPost() {
+		if (this.fieldType != null)
 			fieldTypeEnum = FieldType.setValue(this.fieldType);
-	}
-
-	public static List<Field> findByFieldset(String fieldsetId) {
-		try {
-			Long id = Long.parseLong(fieldsetId);
-			Fieldset fieldset = Fieldset.dao.findById(id);
-			if (fieldset != null) {
-				CriteriaBuilder criteriaBuilder = dao.getCriteriaBuilder();
-				CriteriaQuery<Field> query = criteriaBuilder
-						.createQuery(Field.class);
-				Root<Field> root = query.from(Field.class);
-				query.where(criteriaBuilder.equal(root.get(Field_.fieldset),
-						fieldset));
-				query.orderBy(criteriaBuilder.asc(root.get("position")));
-				return dao.findAllBy(query);
-			}
-		} catch (Exception e) {
-		}
-		return null;
-	}
-
-	public static Map<String, String> options(String fieldsetId) {
-		List<Field> fields = findByFieldset(fieldsetId);
-		Map<String, String> options = dao.options();
-		if (fields != null && !fields.isEmpty()) {
-			Map<String, String> map = new HashMap<String, String>();
-			for (Field field : fields) {
-				String id = field.id.toString();
-				map.put(id, field.toString());
-				options.remove(id);
-			}
-			map.putAll(options);
-			return map;
-		}
-		return options;
 	}
 }
--- app/models/dynamicforms/Fieldset.java
+++ app/models/dynamicforms/Fieldset.java
@@ -9,12 +9,11 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.OneToMany;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
+import javax.persistence.OrderColumn;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import models.helpers.Dao;
 import models.helpers.UserModel;
-import org.hibernate.annotations.IndexColumn;
 import org.hibernate.envers.Audited;
 import play.data.validation.Constraints.*;
 
@@ -32,18 +31,13 @@ public class Fieldset extends UserModel {
 	public String description;
 
 	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "fieldset_id")
-	@IndexColumn(name = "position", base = 1)
+	@JoinColumn(name = "fieldset_id", nullable = false)
+	@OrderColumn(name = "fields_index")
+	@Valid
 	public List<Field> fields = new ArrayList<Field>();
 
 	public String toString() {
 		return name;
-	}
-
-	@PrePersist
-	@PreUpdate
-	private void onPre() {
-		this.fields = Field.dao.getReference(this.fields);
 	}
 
 	public static Map<String, String> options(String formId) {
