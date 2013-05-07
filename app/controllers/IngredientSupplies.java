ec27731b93f9691174e53d929bcef8a71718411a
Sami <hostikka@gmail.com>
2013-05-07 21:27:01 +0300

    Finish ingredient supply

--- app/controllers/IngredientSupplies.java
+++ app/controllers/IngredientSupplies.java
@@ -1,15 +1,9 @@
 package controllers;
 
 import models.IngredientSupply;
-import models.helpers.Dao;
-import models.helpers.Page;
-import play.api.templates.Html;
-import play.api.templates.Template1;
-import play.api.templates.Template2;
 import static play.data.Form.*;
 import play.db.jpa.Transactional;
 import play.mvc.Call;
-import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.With;
 import play.mvc.Security.Authenticated;
