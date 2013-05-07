bd37d9f6bbdf95953949589ab4646fb10877cb79
Sami <hostikka@gmail.com>
2013-05-07 22:33:30 +0300

    Finish batch step 1

--- app/controllers/BatchStep1.java
+++ app/controllers/BatchStep1.java
@@ -1,32 +1,18 @@
 package controllers;
 
 import models.Batch;
-import models.Batch.Step2;
-import models.Ingredient;
-import models.helpers.Dao;
-import models.helpers.Page;
-import play.api.templates.Html;
-import play.api.templates.Template1;
-import play.api.templates.Template2;
 import play.data.Form;
 import static play.data.Form.*;
 import play.db.jpa.Transactional;
-import play.i18n.Messages;
-import play.mvc.Call;
-import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.With;
-import play.mvc.Security.Authenticated;
-import views.html.batches.*;
 import controllers.helpers.Crud;
-import controllers.helpers.UserCrud;
-import controllers.shib.Secured;
 import controllers.shib.Session;
 
 @With(Session.class)
 public class BatchStep1 extends Crud<Batch> {
 	public BatchStep1() {
-		super(null, form(Batch.class), views.html.batches.step1.ref(), null,
+		super(null, form(Batch.class), step1.ref(), null,
 				null, null);
 	}
 
