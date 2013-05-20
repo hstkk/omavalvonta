32f94654fcc770d075fb3adb63506fa206444b55
Sami <hostikka@gmail.com>
2013-05-20 11:01:35 +0300

    Bump version up

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "omavalvonta"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "1.0-SNAPSHOT-1"
 
     val appDependencies = Seq(
 		javaCore,
