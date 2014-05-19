2ab1e845c10a9dc2b5018a5279efc547dfcffa50
htkk <hostikka@gmail.com>
2014-05-19 18:30:36 +0300

    Replace nulls with option

--- app/controllers/Ingredients.java
+++ app/controllers/Ingredients.java
@@ -1,13 +1,22 @@
 package controllers;
 
+import static play.data.Form.form;
 import models.Ingredient;
-import static play.data.Form.*;
+import models.helpers.Dao;
+import models.helpers.Page;
+import play.api.templates.Html;
+import play.api.templates.Template1;
+import play.api.templates.Template2;
+import play.data.Form;
 import play.db.jpa.Transactional;
+import play.libs.F;
 import play.mvc.Call;
 import play.mvc.Result;
-import play.mvc.With;
 import play.mvc.Security.Authenticated;
-import views.html.ingredients.*;
+import play.mvc.With;
+import views.html.ingredients.create;
+import views.html.ingredients.page;
+import views.html.ingredients.update;
 import controllers.helpers.SecuredCrud;
 import controllers.shib.Secured;
 import controllers.shib.Session;
@@ -16,8 +25,15 @@ import controllers.shib.Session;
 @Authenticated(Secured.class)
 public class Ingredients extends SecuredCrud<Ingredient> {
 	public Ingredients() {
-		super(Ingredient.dao, form(Ingredient.class), create.ref(), page.ref(),
-				null, update.ref());
+		super(
+			F.Option.<Dao<Ingredient, Long>>Some(Ingredient.dao),
+			F.Option.<Form<Ingredient>>Some(form(Ingredient.class)),
+			F.Option.<Template1<Form<Ingredient>, Html>>Some(create.ref()),
+			F.Option.<Template1<Page<Ingredient>, Html>>Some(page.ref()),
+			F.Option.<Template1<Ingredient, Html>>None(),
+			F.Option.<Template2<Ingredient, Form<Ingredient>, Html>>Some(update.ref())
+		);
+		
 	}
 
 	@Override
