88d0923c4443dce2e815de02241813318606af90
htkk <hostikka@gmail.com>
2014-05-19 20:27:03 +0300

    Update jpa helper, avoid nulls

--- app/models/helpers/JpaHelper.java
+++ app/models/helpers/JpaHelper.java
@@ -3,16 +3,15 @@ package models.helpers;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-
 import play.Logger;
 import play.Play;
 import play.db.jpa.JPA;
+import com.google.common.base.Optional;
 
 public class JpaHelper<T extends Model, ID extends Serializable> {
 	protected final Class<T> clazz;
@@ -39,25 +38,27 @@ public class JpaHelper<T extends Model, ID extends Serializable> {
 		return JPA.em();
 	}
 
-	public T getReference(ID id) {
+	public Optional<T> getReference(ID id) {
 		try {
-			if (id != null)
-				return getEm().getReference(clazz, id);
+			Optional<ID> optId = Optional.fromNullable(id);
+			if (optId.isPresent())
+				return Optional.fromNullable(getEm().getReference(clazz, optId.get()));
 		} catch (EntityNotFoundException e) {
 		} catch (Exception e) {
 			Logger.warn("getReference", e);
 		}
-		return null;
+		return Optional.absent();
 	}
 
 	public List<T> getReference(List<T> list) {
 		ArrayList<T> references = new ArrayList<T>();
+		Optional<List<T>> optList = Optional.fromNullable(list);
 		try {
-			if (list != null)
-				for (T t : list) {
-					T reference = getReference(t);
-					if (reference != null)
-						references.add(reference);
+			if (optList.isPresent())
+				for (T t : optList.get()) {
+					Optional<T> reference = getReference(t);
+					if (reference.isPresent())
+						references.add(reference.get());
 				}
 		} catch (Exception e) {
 			Logger.warn("getReference", e);
@@ -65,16 +66,12 @@ public class JpaHelper<T extends Model, ID extends Serializable> {
 		return references;
 	}
 
-	public T getReference(T t) {
-		try {
-			if (t != null && t.id != null)
-				return getEm().getReference(clazz, t.id);
-		} catch (EntityNotFoundException e) {
-		} catch (NullPointerException e) {
-		} catch (Exception e) {
-			Logger.warn("getReference", e);
-		}
-		return null;
+	@SuppressWarnings("unchecked")
+	public Optional<T> getReference(T t) {
+		Optional<T> optT = Optional.fromNullable(t);
+		if (optT.isPresent())
+			return getReference((ID) optT.get().id);
+		return Optional.absent();
 	}
 
 	protected TypedQuery<T> setPage(TypedQuery<T> q, Integer pageNumber) {
