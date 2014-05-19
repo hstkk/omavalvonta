cf0ef8ccd301257b487f66383e8bd25fa1139f33
htkk <hostikka@gmail.com>
2014-05-19 18:27:58 +0300

    Replace nulls with option

--- app/controllers/Forms.java
+++ app/controllers/Forms.java
@@ -1,12 +1,22 @@
 package controllers;
 
-import static play.data.Form.*;
+import static play.data.Form.form;
+import models.helpers.Dao;
+import models.helpers.Page;
+import play.api.templates.Html;
+import play.api.templates.Template1;
+import play.api.templates.Template2;
+import play.data.Form;
 import play.db.jpa.Transactional;
+import play.libs.F;
 import play.mvc.Call;
 import play.mvc.Result;
-import play.mvc.With;
 import play.mvc.Security.Authenticated;
-import views.html.forms.*;
+import play.mvc.With;
+import views.html.forms.create;
+import views.html.forms.page;
+import views.html.forms.show;
+import views.html.forms.update;
 import controllers.helpers.SecuredCrud;
 import controllers.shib.Secured;
 import controllers.shib.Session;
@@ -15,9 +25,14 @@ import controllers.shib.Session;
 @Authenticated(Secured.class)
 public class Forms extends SecuredCrud<models.dynamicforms.Form> {
 	public Forms() {
-		super(models.dynamicforms.Form.dao,
-				form(models.dynamicforms.Form.class), create.ref(), page.ref(),
-				show.ref(), update.ref());
+		super(
+			F.Option.<Dao<models.dynamicforms.Form, Long>>Some(models.dynamicforms.Form.dao),
+			F.Option.<Form<models.dynamicforms.Form>>Some(form(models.dynamicforms.Form.class)),
+			F.Option.<Template1<Form<models.dynamicforms.Form>, Html>>Some(create.ref()),
+			F.Option.<Template1<Page<models.dynamicforms.Form>, Html>>Some(page.ref()),
+			F.Option.<Template1<models.dynamicforms.Form, Html>>Some(show.ref()),
+			F.Option.<Template2<models.dynamicforms.Form, Form<models.dynamicforms.Form>, Html>>Some(update.ref())
+		);
 	}
 
 	@Override
